d6b0eceb7793b7302ea505883f444e2c5a6940ff
vagrant <vagrant@homestead>
2020-06-11 02:04:41 +0000

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,15 +13,15 @@ class DatabaseSeeder extends Seeder
     {
         if(env('APP_ENV') == 'local')
         {
-            $this->call(UserTableSeeder::class);
-            $this->call(StatusTableSeeder::class);
-            $this->call(BuyTableSeeder::class);
-            $this->call(SaleTableSeeder::class);
-            $this->call(FinanceTableSeeder::class);
-            $this->call(BuildingTableSeeder::class);
-            $this->call(ShippingTableSeeder::class);
-            $this->call(DeliveryTableSeeder::class);
-            $this->call(StatusTableSeeder::class);
+            // $this->call(UserTableSeeder::class);
+            // $this->call(StatusTableSeeder::class);
+            // $this->call(BuyTableSeeder::class);
+            // $this->call(SaleTableSeeder::class);
+            // $this->call(FinanceTableSeeder::class);
+            // $this->call(BuildingTableSeeder::class);
+            // $this->call(ShippingTableSeeder::class);
+            // $this->call(DeliveryTableSeeder::class);
+            // $this->call(StatusTableSeeder::class);
             $this->call(AddVerificarStatusSeeder::class);
         }
     }
